02a890d7e0f0ddfb44485879cda9215b357bfdad
a-fashola01 <128617918+a-fashola01@users.noreply.github.com>
2024-09-14 17:50:32 -0400

    Constants naming convention, variables location cleanup and comments clean up

--- Tile_cost_calculator_program_0/Program.cs
+++ Tile_cost_calculator_program_0/Program.cs
@@ -1,7 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System;
 
-namespace Module_0
+namespace Tile_cost_calculator_program_0
 {
     internal class Program
     {
@@ -10,18 +10,14 @@ namespace Module_0
         static void Main(string[] args)
         {
             //create constant variables for shapes
+            const double PI = 3.1415926535897931;
+            const double COST_PER_HOUR_LABOUR = 86.00;
+            const double AVG_FLOORING = 20.00;
+            
+            //Create lists for shapes
             List<string> circles = new List<string>();
             List<string> rectangles = new List<string>();
             List<string> triangles = new List<string>();
-            
-            //define calculation variables
-            const double PI = 3.1415926535897931;
-            const double cost_per_hour_l = 86.00;
-            const double avg_flooring = 20.00;
-            double width; 
-            double result;
-            double sq_ft = 0;
-            double labour_cost = 0;
                         
             //add values to variables
             circles.Add("circle");
@@ -48,6 +44,10 @@ namespace Module_0
             
             //Console.WriteLine(shape);
             
+            //define calculation variables
+            double width; 
+            double sq_ft = 0;
+            
             if (rectangles.Contains(shape))
             {
                 double length;
@@ -57,12 +57,7 @@ namespace Module_0
                 Console.WriteLine("Please enter the width:");
                 width = Convert.ToDouble(Console.ReadLine());
                 sq_ft = length * width;
-
-                /*result = sq_ft * Cost_per_foot;
-                labour_cost = (sq_ft / avg_flooring) *cost_per_hour_l;
-                Console.WriteLine("The cost to cover this area is: $" + result);
-                Console.WriteLine("The labour cost is: $" + labour_cost);
-                Console.WriteLine("The total cost is: $" + (labour_cost + result));*/
+                
             }
 
             if (circles.Contains(shape))
@@ -72,11 +67,6 @@ namespace Module_0
                 radius = Convert.ToDouble(Console.ReadLine());
                 sq_ft = (PI * Math.Pow(radius, 2)) ;
                 
-                /*result = sq_ft * Cost_per_foot;
-                labour_cost = (sq_ft / avg_flooring) *cost_per_hour_l;
-                Console.WriteLine("The cost to cover this area is: $" + result);
-                Console.WriteLine("The labour cost is: $" + labour_cost);
-                Console.WriteLine("The total cost is: $" + (labour_cost + result));*/
             }
             
             if (triangles.Contains(shape))
@@ -88,19 +78,17 @@ namespace Module_0
                 Console.WriteLine("Please enter the width:");
                 width = Convert.ToDouble(Console.ReadLine());
                 sq_ft = ((height * width) / 2);
-                
-                /*result = sq_ft * Cost_per_foot;
-                labour_cost = (sq_ft / avg_flooring) *cost_per_hour_l;
-                Console.WriteLine("The cost to cover this area is: $" + result);
-                Console.WriteLine("The labour cost is: $" + labour_cost);
-                Console.WriteLine("The total cost is: $" + (labour_cost + result));*/
+           
             }
             
-            result = Math.Round((sq_ft * Cost_per_foot),2);
-            labour_cost = Math.Round(((sq_ft / avg_flooring) * cost_per_hour_l), 2);
-            double final_cost = Math.Round((labour_cost + result), 2);
+            double labour_cost = 0;
+            double cost_per_sq_ft;
+            
+            cost_per_sq_ft = Math.Round((sq_ft * Cost_per_foot),2);
+            labour_cost = Math.Round(((sq_ft / AVG_FLOORING) * COST_PER_HOUR_LABOUR), 2);
+            double final_cost = Math.Round((labour_cost + cost_per_sq_ft), 2);
                 
-            Console.WriteLine($"The cost to cover this area is: ${result}" );
+            Console.WriteLine($"The cost to cover this area is: ${cost_per_sq_ft}" );
             Console.WriteLine($"The labour cost is: ${labour_cost}");
             Console.WriteLine($"The total cost is: ${final_cost}" );
             
